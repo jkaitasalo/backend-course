364f890c132f9d526d1aee4cbc629fe451f259de
Jouni Kaitasalo <jouni.kaitasalo@hotmail.com>
2024-08-26 14:38:11 +0300

    first commit

---
+++ src/main/java/bgx151/s24/S24Application.java
@@ -0,0 +1,13 @@
+package bgx151.s24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S24Application.class, args);
+	}
+
+}
---
+++ src/test/java/bgx151/s24/S24ApplicationTests.java
@@ -0,0 +1,13 @@
+package bgx151.s24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
